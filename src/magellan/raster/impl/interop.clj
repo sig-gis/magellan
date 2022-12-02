999b10b112134b6c114ca1b708b7d21059323666
Valentin Waeselynck <val.vvalval@gmail.com>
2022-12-02 19:23:30 +0100

    Refactored magellan.raster.impl.interop: multimethods -> case. (PR#27).

--- src/magellan/raster/impl/interop.clj
+++ src/magellan/raster/impl/interop.clj
@@ -1,44 +1,20 @@
 (ns magellan.raster.impl.interop
   (:import (java.awt.image Raster)))
 
-;; NOTE: the overhead of multimethod invocation is typically insignificant, given that we call these once per band.
-(defmulti tensor-backing-array (fn [target-dtype _arr-length] target-dtype))
+(defn tensor-backing-array
+  [target-dtype arr-length]
+  (let [arr-length (int arr-length)]
+    (case target-dtype
+      :int32   (int-array arr-length)
+      :float32 (float-array arr-length)
+      :float64 (double-array arr-length))))
 
-(defmulti get-samples (fn [target-dtype _raster _x _y _w _h _b] target-dtype))
-
-;; NOTE we could use macros to eliminate more repetition in this code, (Val, 29 Nov 2022)
-;; but it doesn't seem worth it: that would make the code more magical,
-;; less tangible, and less editor-friendly.
-
-(defmethod tensor-backing-array :int32
-  [_target-dtype arr-length]
-  (int-array (int arr-length)))
-
-(defmethod get-samples :int32
-  [_target-dtype ^Raster raster x y w h b]
+(defn get-samples
+  [target-dtype ^Raster raster x y w h b]
   (let [w            (int w)
         h            (int h)
         chunk-length (* w h)]
-    (.getSamples raster (int x) (int y) w h (int b) (int-array chunk-length))))
-
-(defmethod tensor-backing-array :float32
-  [_target-dtype arr-length]
-  (float-array (int arr-length)))
-
-(defmethod get-samples :float32
-  [_target-dtype ^Raster raster x y w h b]
-  (let [w            (int w)
-        h            (int h)
-        chunk-length (* w h)]
-    (.getSamples raster (int x) (int y) w h (int b) (float-array chunk-length))))
-
-(defmethod tensor-backing-array :float64
-  [_target-dtype arr-length]
-  (double-array (int arr-length)))
-
-(defmethod get-samples :float64
-  [_target-dtype ^Raster raster x y w h b]
-  (let [w            (int w)
-        h            (int h)
-        chunk-length (* w h)]
-    (.getSamples raster (int x) (int y) w h (int b) (double-array chunk-length))))
+    (case target-dtype
+      :int-32  (.getSamples raster (int x) (int y) w h (int b) (int-array chunk-length))
+      :float32 (.getSamples raster (int x) (int y) w h (int b) (float-array chunk-length))
+      :float64 (.getSamples raster (int x) (int y) w h (int b) (double-array chunk-length)))))
